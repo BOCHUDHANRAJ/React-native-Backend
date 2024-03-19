e0226199067a0446dcd7f2ed2c01b8c89412730e
BOCHU DHANRAJ <88509660+BOCHUDHANRAJ@users.noreply.github.com>
2024-03-19 15:28:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
-const PORT =process.env.PORT || 4000; 
+const PORT =process.env.PORT || 5000; 
 
 app.listen(PORT,()=>{
     console.log(`Server is running at PORT ${PORT}` );
